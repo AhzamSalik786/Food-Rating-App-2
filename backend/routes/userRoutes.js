5ef6016d63629399be3332d0a9cfdca75af38d6b
Ahzam Salik <ahzamsalik786@gmail.com>
2021-02-05 18:33:45 +0500

    first change

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express'
+const router = express.Router()
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+} from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
+router.route('/').post(registerUser)
+router.post('/login', authUser)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
+
+// router.post('/login', (req, res) => {
+//   authUser(req, res);
+// });
+
+export default router
